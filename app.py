c6579cd8dfa32bbb88294954fbc3924057626537
Arjun Sarao <arjunsarao727@gmail.com>
2024-05-13 21:55:28 -0400

    updates

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
-import streamlit as st
 import os
-from pydantic.v1 import SecretStr
-from dotenv import load_dotenv
 
-from components.template import Template
-from components.cover_letter import CoverLetterContents
-from components.personal_info import PersonalInfo
-
-from langchain_openai import ChatOpenAI
-from langchain_community.document_loaders import UnstructuredFileIOLoader
-from unstructured.cleaners.core import clean_extra_whitespace
-from langchain.output_parsers import PydanticOutputParser
+import streamlit as st
 from langchain.prompts import PromptTemplate
-
-load_dotenv(dotenv_path=".env")
+from langchain_community.document_loaders import UnstructuredFileIOLoader
+from langchain_openai import ChatOpenAI
+from unstructured.cleaners.core import clean_extra_whitespace
 
 
 def main() -> None:
-    API_KEY: SecretStr = SecretStr(os.getenv(key="OPENAI_API_KEY", default="BRUH"))
+    API_KEY = "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"
     st.title("makeCV")
 
     if "generated_cover_letter" not in st.session_state:
@@ -30,7 +21,6 @@ def main() -> None:
     )
 
     if resume and job_listing_text:
-        # Load the resume
         loader = UnstructuredFileIOLoader(
             file=resume,
             mode="elements",
@@ -42,25 +32,19 @@ def main() -> None:
         resume_text = "".join([doc.page_content for doc in docs])
         st.toast("Resume Uploaded Successfully", icon="✅")
 
-        # Initialize LLM + Prompt + Parser
 
-        llm = ChatOpenAI(
-            model="gpt-3.5-turbo", api_key=API_KEY, max_tokens=1000
-        )  # Change model to 4 later
+        llm = ChatOpenAI(model="gpt-4", api_key=API_KEY, max_tokens=2048)
 
-        parser = PydanticOutputParser(pydantic_object=CoverLetterContents)
         prompt = PromptTemplate(
             input_variables=["job_listing_text", "resume_text"],
             template=r"""
-            Please write the paragraphs of a cover letter formatted in plaintext for the following job listing and resume. \
-            Give me only the letter, no preamble nor any other text. Enclose any library/developer tool in \emph{{}}. \
+            Please write a cover letter formatted in markdown for the following job listing and resume. \
+            Give me only the letter, no preamble nor any other text. \
             Use only experiences, projects, and skills that are directly contained in the resume. \
             The general format is as follows:
             1. The first paragraph should be a few (1-3) sentences summarizing what the company does.
             2. A paragraph about how my experience/projects/skills would be an asset to the company.
             The overall length should be very close to, but not exceed, 500 words. DO NOT include any information like my email or address in the header of the letter. DO NOT end with a sign-off.
-            Format Instructions:
-            {format_instructions}
 
             Job Listing:
             {job_listing_text}\n
@@ -68,31 +52,27 @@ def main() -> None:
             Resume:
             {resume_text}
             """,
-            partial_variables={"format_instructions": parser.get_format_instructions()},
         )
 
-        t = Template("templates/deedy/deedy.tex")
-
-        chain = prompt | llm | parser
+        chain = prompt | llm
 
         if not st.session_state["generated_cover_letter"]:
-            out: CoverLetterContents = chain.invoke(
+            out = chain.invoke(
                 {
                     "job_listing_text": job_listing_text,
                     "resume_text": resume_text,
                 }
             )
+            st.session_state["generated_cover_letter"] = out
 
-        t.populate(cv=out, pi=PersonalInfo().from_yaml(fp="personal_info.yaml"))
+        st.markdown(st.session_state["generated_cover_letter"].content, unsafe_allow_html=True)
 
-        st.code(t.contents, language="latex", line_numbers=True)
-
-        st.download_button(
-            label="Download Cover Letter",
-            type="primary",
-            data=t.contents,
-            file_name="cover_letter.tex",
-        )
+        # st.download_button(
+        #     label="Download Cover Letter",
+        #     type="primary",
+        #     data=st.session_state["generated_cover_letter"],
+        #     file_name="cover_letter.md",
+        # )
 
 
 if __name__ == "__main__":
---
+++ coverletter.md
@@ -0,0 +1,29 @@
+# Cover Letter
+
+Arjun Sarao  
+arjun.sarao@uwaterloo.ca  
+[LinkedIn](linkedin.com/in/arjun-sarao) | [GitHub](github.com/DarkHawk727)
+
+November 30, 2023
+
+Qualcomm Canada ULC  
+Interns Group, Interns Group  
+Interim Engineering Intern - HW
+
+Dear Hiring Manager,
+
+I am writing to express my keen interest in the Interim Engineering Intern - HW position at Qualcomm Canada ULC, as listed on your careers page. As a current Bachelor of Computer Science student at the University of Waterloo with a strong focus on machine learning and web development, I am excited about the opportunity to contribute to Qualcomm’s innovative projects, particularly in the areas of deep learning and neural network architecture.
+
+Your emphasis on inventing technologies for a connected future resonates deeply with my professional pursuits and academic background. At Starai Tutoring, I honed my skills in JavaScript and React.js while developing a responsive multi-page website. This experience aligns with Qualcomm’s commitment to leading-edge technology and inventive solutions.
+
+My project experience, including the Deep Work Tracker and the Personal AI web applications, demonstrates my proficiency in Python and neural networks, key requirements for the role. Specifically, my use of GPT-4 in the Personal AI project speaks directly to your need for experience with auto-regressive language models. Additionally, my involvement in a wide range of machine learning areas, as evidenced by my completion of Machine Learning and Deep Learning Specializations on Coursera, has equipped me with a strong foundation in Pytorch and neural network architecture development, aligning well with the job’s minimum qualifications.
+
+I am particularly excited about the possibility of contributing to Qualcomm’s exploration of end-to-end learning in neural networks. My educational journey at the University of Waterloo and my self-learning initiatives have been driven by a similar passion for exploring the frontiers of AI and machine learning.
+
+Moreover, my personal achievements in Hackathons, including being a 2x Google Cloud Prize Recipient, showcase my innovative thinking and ability to deliver results in competitive environments, qualities I understand are valued at Qualcomm.
+
+I am available for either the Winter or Summer Co-op terms and am enthusiastic about the possibility of joining Qualcomm, a pioneer in driving the technologies of the future. Thank you for considering my application. I look forward to the opportunity to discuss how my skills, experience, and passion for technology align with the needs of Qualcomm.
+
+Sincerely,
+
+Arjun Sarao
